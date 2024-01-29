ede6da31076931b95c1f1cdab822c3fb6b5576e0
Srđan Stjepanović <srdjan.stjepanovic01@gmail.com>
2024-01-29 14:58:09 +0100

    added live action classification on video using mediapipe

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pickle
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from openpose import draw_openpose
+from data_preprocessing import classes
 import sys
 
 
@@ -49,7 +50,7 @@ def process_image_openpose(img_path, model, model_type):
     draw_openpose(img_path, model, model_type)
 
 
-def mediapipe_pose_detection():
+def mediapipe_pose_detection(model_type, model):
     mp_pose = mp.solutions.pose
     pose = mp_pose.Pose()
     mp_drawing = mp.solutions.drawing_utils
@@ -66,6 +67,24 @@ def mediapipe_pose_detection():
         if results.pose_landmarks:
             mp_drawing.draw_landmarks(
                 frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+            row = []
+            landmarks = results.pose_landmarks.landmark
+            for landmark in landmarks:
+                height, width, _ = rgb_frame.shape
+                x, y = landmark.x * width, landmark.y * height
+                row += [x, y]
+            if model_type != 'nn':
+                row = np.array(row).reshape(1, -1)
+                prediction = model.predict(row)
+                class_name = classes[prediction[0]]
+            else:
+                row = np.array(row).reshape(- 1, 33, 2)
+                prediction = model.predict(row)
+                class_name = classes[np.argmax(prediction)]
+            
+            cv.rectangle(frame, (0, 0), (150, 40), (0, 0, 0), -1)
+            cv.putText(frame, class_name, (2, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
         
         cv.imshow("Pose Detection", frame)
 
@@ -79,7 +98,9 @@ if __name__=='__main__':
     isOpenPose = True
     if sys.argv[1].lower().strip() == 'false':
         isOpenPose = False
-        mediapipe_pose_detection()
+        model_type = sys.argv[2].lower().strip()
+        model = load_classificator(model_type, isOpenPose)
+        mediapipe_pose_detection(model_type, model)
     else:
         model_type = sys.argv[2].lower().strip()
         print(len(model_type))
